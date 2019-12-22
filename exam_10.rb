e83ac79c4c5956f439a5e5dbf7f1311173f28c1e
OsamaOmar322 <57850788+OsamaOmar322@users.noreply.github.com>
2019-12-22 05:24:32 +0000

    Add files via upload

--- exam_10.rb
+++ exam_10.rb
@@ -3,7 +3,18 @@
 # A yelled word is when each word is all uppercase followed by 2 exclamation marks (!!)
 
 def deaf_grandma(sentence)
-    # Write your code here
-end
+    uppercase_sentence = sentence.upcase 
+    #array = sentence.split
+    for i in 0..sentence.length
+     uppercase_sentence.collect! { |element|
+        (element) "#{element}!!"
+     }   
+    end 
+end 
 
 puts deaf_grandma("I have a bad feeling about this") == "I!! HAVE!! A!! BAD!! FEELING!! ABOUT!! THIS!!"
+
+x = %w(hello there world)
+x.collect! { |element|
+  (element == "hello") ? "hi" : element
+}
